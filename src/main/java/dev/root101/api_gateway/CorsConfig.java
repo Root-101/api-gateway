beec208452a0ce42aafc036c664639590bfef8ee
Jesus Hernandez Barrios <jhernandezb96@gmail.com>
2024-11-01 17:40:47 -0400

    Add cors config

---
+++ src/main/java/dev/root101/api_gateway/CorsConfig.java
@@ -0,0 +1,18 @@
+package dev.root101.api_gateway;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+public class CorsConfig implements WebFluxConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .exposedHeaders("Authorization", "Content-Type");
+    }
+}
