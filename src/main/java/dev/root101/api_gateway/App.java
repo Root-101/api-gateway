a5e49f5e9ceb9f08d3b862994d4ecfc65b71f8ea
Jesus Hernandez Barrios <jhernandezb96@gmail.com>
2024-11-19 21:08:25 -0500

    Pop  stash

--- src/main/java/dev/root101/api_gateway/App.java
+++ src/main/java/dev/root101/api_gateway/App.java
@@ -10,24 +10,59 @@ public class App {
         System.setProperty("PROFILE", "DEV");
         System.setProperty("PORT", "8080");
         System.setProperty("APPLICATION_NAME", "Test Api-Gateway");
-        /*System.setProperty("ROUTE_CONFIG", """
+        System.setProperty("ROUTE_CONFIG", """
                                            [
-                                              {
-                                                 "id":"poc-spring-back",
-                                                 "path":"/poc/**",
-                                                 "uri":"https://api.zyrcled.com",
-                                                 "replaceFrom":"/poc/",
-                                                 "replaceTo":"/"
-                                              },
-                                              {
-                                                 "id":"zyrcled-notification-dev",
-                                                 "path":"/notification/**",
-                                                 "uri":"https://push-notification-dev.up.railway.app",
-                                                 "replaceFrom":"/notification/",
-                                                 "replaceTo":"/"
-                                              }
-                                           ]
-                                           """);*/
+                                   {
+                                     "id": "auth-dev",
+                                     "path": "/auth/**",
+                                     "uri": "https://back-authentication-dev.up.railway.app",
+                                     "replaceFrom": "/auth/",
+                                     "replaceTo": "/"
+                                   },
+                                   {
+                                     "id": "stories-dev",
+                                     "path": "/stories/**",
+                                     "uri": "https://stories-gateway-2.up.railway.app",
+                                     "replaceFrom": "/stories/",
+                                     "replaceTo": "/"
+                                   },
+                                   {
+                                     "id": "notifications-dev",
+                                     "path": "/notifications/**",
+                                     "uri": "https://push-notification-dev.up.railway.app",
+                                     "replaceFrom": "/notifications/",
+                                     "replaceTo": "/"
+                                   },
+                                   {
+                                     "id": "ads-dev",
+                                     "path": "/ads/**",
+                                     "uri": "https://ads-control-dev.up.railway.app",
+                                     "replaceFrom": "/ads/",
+                                     "replaceTo": "/"
+                                   },
+                                   {
+                                     "id": "intl-dev",
+                                     "path": "/intl/**",
+                                     "uri": "https://intl-dev.up.railway.app",
+                                     "replaceFrom": "/intl/",
+                                     "replaceTo": "/"
+                                   },
+                                   {
+                                     "id": "onboarding-dev",
+                                     "path": "/onboarding/**",
+                                     "uri": "https://onboarding-dev.up.railway.app",
+                                     "replaceFrom": "/onboarding/",
+                                     "replaceTo": "/"
+                                   },
+                                   {
+                                     "id": "users-dev",
+                                     "path": "/users/**",
+                                     "uri": "https://users-dev.up.railway.app",
+                                     "replaceFrom": "/users/",
+                                     "replaceTo": "/"
+                                   }
+                                 ]
+                """);
 
         SpringApplication.run(App.class, args);
     }
