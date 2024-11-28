190a6233e461c2893ef5346c0b819dad1a02b657
Jesus Hernandez Barrios <jhernandezb96@gmail.com>
2024-11-27 20:51:37 -0500

    Improvement [route-controller]: add prefix '_admin'

--- src/main/java/dev/root101/api_gateway/feature/controller/RoutesController.java
+++ src/main/java/dev/root101/api_gateway/feature/controller/RoutesController.java
@@ -11,7 +11,7 @@ import reactor.core.publisher.Mono;
 import java.util.List;
 
 @RestController
-@RequestMapping("/routes")
+@RequestMapping("/_admin/routes")
 public class RoutesController {
 
     private final DynamicRouteService dynamicRouteService;
