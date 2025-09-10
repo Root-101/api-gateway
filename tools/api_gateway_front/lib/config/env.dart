bd74887c89e414f2e69476462719970b15861a9b
Jesus Hernandez Barrios <jhernandezb96@gmail.com>
2025-09-10 10:32:08 -0600

    Impr [tools/front][env]: Fix env config to handle gateway url that start with http
    If the gateway url (GATEWAY_SERVICE_URL env) start with http, its used literal, if not, its added the `https://` in front

--- tools/api_gateway_front/lib/config/env.dart
+++ tools/api_gateway_front/lib/config/env.dart
@@ -15,13 +15,18 @@ extension EnvConfigExtension on EnvConfig {
 
 ///use it like this
 class Environment {
-  static String get gatewayUrl => 'https://${env.GATEWAY_SERVICE_URL}';
+  static String get gatewayUrl => env.GATEWAY_SERVICE_URL.startsWith('http')
+      ? env.GATEWAY_SERVICE_URL
+      : 'https://${env.GATEWAY_SERVICE_URL}';
 
-  static String get baseApiUrl =>
-      'https://${env.GATEWAY_SERVICE_URL}/${env.ADMIN_PATH}';
+  static String get baseApiUrl => env.GATEWAY_SERVICE_URL.startsWith('http')
+      ? '${env.GATEWAY_SERVICE_URL}/${env.ADMIN_PATH}'
+      : 'https://${env.GATEWAY_SERVICE_URL}/${env.ADMIN_PATH}';
 
   static Map<String, String> get raw => {
     'GATEWAY_SERVICE_URL': env.GATEWAY_SERVICE_URL,
     'ADMIN_PATH': env.ADMIN_PATH,
+    'gatewayUrl': gatewayUrl,
+    'baseApiUrl': baseApiUrl,
   };
 }
