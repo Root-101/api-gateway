6cd9729300f9c11070188282474f02055c62c2df
Jesús Hernández Barrios <jhernandezb96@gmail.com>
2026-05-17 16:43:52 -0600

    Impr [routes]: Add missing import

--- src/main/java/dev/root101/api_gateway/features/routes/logic/model/RouteConfigRequest.java
+++ src/main/java/dev/root101/api_gateway/features/routes/logic/model/RouteConfigRequest.java
@@ -1,6 +1,7 @@
 package dev.root101.api_gateway.features.routes.logic.model;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
